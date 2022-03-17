609e91db7bb46a4aa0b66faeb58ccf602bbbbf33
YuyuliyaPanarina <101624415+YuyuliyaPanarina@users.noreply.github.com>
2022-03-17 18:49:53 +0300

    Add files via upload

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,6 +16,17 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
+    if (productsSum == 0) {
+        console.log(shippingSum=0);
+    }
+    if (productsSum >= freeShippingMinSum) {
+        console.log(shippingSum=0);
+    }
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        console.log(shippingSum=shippingPrice);
+    }
+
+
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -33,6 +44,10 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
+    
+    if (productsSum >= discountMinSum) {
+        console.log(discountSum = productsSum / 100 * discountPart);
+    } else if (discountSum = 0);
 
     // Конец решения задания №2.2.
 
@@ -46,17 +61,27 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
-
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
+
+    totalSum = productsSum;
+    totalSum=totalSum-discountSum;
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
 
+    totalSum=totalSum+shippingSum;
+
+
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    let freeShipping;
+    shippingSum == 0;
+    freeShipping = !shippingSum;
+    
+
 
     // Конец решения задачи №2.3.
 
